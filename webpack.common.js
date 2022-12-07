87882d7bd825b086c369c717a5b276d7b69c775b
rickkun11 <rickkun11@gmail.com>
2022-12-07 08:19:48 +0700

    init

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/scripts/index.js'),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          exclude: '/node_modules/',
+          use: [
+                  {
+                        loader: 'style-loader',
+                  },
+                  {
+                        loader: 'css-loader',
+                  },
+                ],
+            },
+
+            {
+              test: /\.(png|svg|jpg|jpeg|gif|webp)$/,
+              use: [
+                'file-loader',
+              ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src/templates/index.html"),
+            filename: "index.html",
+        }),
+
+    ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,57 @@
+const {merge} = require('webpack-merge');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+const common = require('./webpack.common');
+//optimization
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              generatorOpts: { compact: false },
+            },
+          },
+        ],
+      }
+    ],
+  },
+  plugins: [
+
+    new MiniCssExtractPlugin({filename: 'assets/css/[hash].css'}),
+    // new BundleAnalyzerPlugin(),
+    new InjectManifest({
+      swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
+      swDest: path.resolve(__dirname, 'dist/sw.js'),
+    }),
+
+  ],
+
+  optimization: {
+    minimizer: [
+
+      new ImageMinimizerPlugin({
+        minimizer: {
+
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["optipng", { optimizationLevel: 5 }]
+            ],
+          },
+        },
+      }),
+    ],
+
+  },
+});
